66b338331d4d263fd0ca0a8e4d7846fba82f2ed1
Zwb <ethercflow@gmail.com>
2018-12-10 16:36:50 +0800

    chore(deploy): do some cleanup and compile analyzer

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
+if [ -d "bin" ]; then
+  rm -rf bin
+fi
+
 mkdir bin
 
 make -C module
+if [ -n `lsmod | grep trace_wperf_events.ko` ]; then
+  sudo rmmod trace_wperf_events.ko
+fi
 cd module && sudo insmod trace_wperf_events.ko
 
 cd ..
 
 make -C recorder
 mv recorder/recorder bin/
+
+backup=`echo $GOPATH`
+export GOPATH=`pwd`/analyzer
+cd ./analyzer/src/analyzer && go build
+mv analyzer ../../../bin
+cd ../../../
+export GOPATH=$backup
 
 cp scripts/*.py bin/
